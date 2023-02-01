5817951d17ebcaf21332ef46b856e2030475f277
kegedy <kegedy@gmail.com>
2023-02-01 12:47:41 -0800

    Wed Feb  1 12:47:41 PM PST 2023

---
+++ _build/jupyter_execute/pcb/projects/ISL6236A.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # ISL6236A
+
+# ![](ISL6236A/ISL6236A-all-layers.png)
---
+++ _build/jupyter_execute/pcb/projects/ISL6236A/ISL6236A.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # ISL6236A
+
+# ![](ISL6236A-all-layers.png)
+
+# In[ ]:
+
+
+
+
