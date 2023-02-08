6d274170405493af8a699ad54c82ff7e5c64dce0
kegedy <kegedy@gmail.com>
2023-02-07 23:51:20 -0800

    Tue Feb  7 11:51:20 PM PST 2023

---
+++ _build/jupyter_execute/linear-integrated-circuits/single-stage.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Single Stage Summary
+
+# |                | Common Source | Common Source with $R_S$ | Common Gate       | Common Gate with $R_S$ | Common Drain | Common Drain with $R_D$ |
+# | :------------- | :------------: | :-----------------------: | :----------------: | :---------------------: | :-----------: | :----------------------: |
+# | $R_{in}$      |     $\infty$ |                $\infty$ | $\dfrac{1}{g_m}$ | $\dfrac{1}{g_m}$      | $\infty$    | $\infty$               |
+# | $R_{out}$     |               |                          |                   |                        |              |                         |
+# | $A_v$          |               |                          |                   |                        |              |                         |
+# | $\omega_H$    |               |                          |                   |                        |              |                         |
+# | $\omega_L$    |               |                          |                   |                        |              |                         |
+# | Gain Bandwidth |               |                          |                   |                        |              |                         |
+
+# In[ ]:
+
+
+
+
---
+++ _build/jupyter_execute/non-linear-systems/desensitization.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Desensitization
+
+# [Drawbacks of Non Linear Systems: Desensitization Effect](https://rahsoft.com/2021/04/27/drawbacks-of-non-linear-systems-desensitization-effect/)
+
+# In[ ]:
+
+
+
+
